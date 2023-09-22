941c84fb34f92f191cd10306fe90a5ddb674f1ab
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-09-22 17:49:53 +0800

    Create kaoyan.js

---
+++ kaoyan.js
@@ -0,0 +1,15 @@
+/******************************
+微信小程序考研数学欧几里得
+脚本作者ios15 9.22更新
+
+
+[rewrite_local]
+#^https?:\/\/api\.tumukaoyan\.com\/api\/headimg_list\/GetListByAll url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/kaoyanxcx.js
+
+^https?:\/\/api\.tumukaoyan\.com\/api\/wx\/CheckCodeV2? url script-response-body https://raw.githubusercontent.com/Yu9191/Script/main/kaoyan.js
+
+
+
+[mitm]
+hostname = api.tumukaoyan.com
+*******************************/
