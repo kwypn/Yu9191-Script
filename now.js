08f44a0e20b83e95a01752083b9db9763151464e
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-10-14 16:28:28 +0800

    Update now.js

--- now.js
+++ now.js
@@ -3,13 +3,13 @@ var obj = JSON.parse(body);
 
 obj.result.now_vip.is_forever = true,
 obj.result.now_vip.is_vip = true,  
-obj.result.now_vip.expire_time = 1871093782,  
+obj.result.now_vip.expire_time = 490001314520000,  
   
 obj.result.yoga.is_vip = true,  
-obj.result.yoga.expire_time = 1871093782,
+obj.result.yoga.expire_time = 490001314520000,
   
 obj.result.excellent.is_vip = true,  
-obj.result.excellent.expire_time = 1871093782,    
+obj.result.excellent.expire_time = 490001314520000,    
   
 
 body = JSON.stringify(obj);
